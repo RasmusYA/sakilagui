2c5a023a4e4d8062e0c23770ab5de3a2b2e6888a
Rasmus Petersson <rasmus.petersson1@yaelev.se>
2022-03-30 15:30:52 +0200

    Add new payment class

---
+++ src/main/java/com/grupp2/sakilagui/Payment.java
@@ -0,0 +1,74 @@
+package com.grupp2.sakilagui;
+
+import java.sql.Date;
+
+public class Payment {
+
+    private int payment_id;	        //smallint(5) unsigned
+    private int customer_id;	    //	smallint(5) unsigned
+    private int staff_id;	        //	tinyint(3) unsigned
+    private int rental_id;	        //	int(11)
+    private double amount;	            //	decimal(5,2)
+    private Date payment_date;	    //	datetime
+    private Date last_update;	    //	timestamp
+
+    public Payment(){
+
+    }
+    
+    public int getPayment_id() {
+        return payment_id;
+    }
+
+    public void setPayment_id(int payment_id) {
+        this.payment_id = payment_id;
+    }
+
+    public int getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(int customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public int getStaff_id() {
+        return staff_id;
+    }
+
+    public void setStaff_id(int staff_id) {
+        this.staff_id = staff_id;
+    }
+
+    public int getRental_id() {
+        return rental_id;
+    }
+
+    public void setRental_id(int rental_id) {
+        this.rental_id = rental_id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getPayment_date() {
+        return payment_date;
+    }
+
+    public void setPayment_date(Date payment_date) {
+        this.payment_date = payment_date;
+    }
+
+    public Date getLast_update() {
+        return last_update;
+    }
+
+    public void setLast_update(Date last_update) {
+        this.last_update = last_update;
+    }
+}
