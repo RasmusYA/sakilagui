329916472f91c6f2fe99b15a0f6b41d13d0b036b
Joan-barcelona <32038566+Joan-barcelona@users.noreply.github.com>
2022-04-07 17:23:41 +0200

    Revert "Working in CustomerDAO"

--- src/main/java/com/grupp2/sakilagui/CustomerDAO.java
+++ src/main/java/com/grupp2/sakilagui/CustomerDAO.java
@@ -1,42 +1,108 @@
 package com.grupp2.sakilagui;
 
-import com.grupp2.sakilagui.bs.Customer;
-
 import javax.persistence.*;
 import java.sql.Date;
-import java.util.List;
 
-
+@Entity
+@Table(name="customer")
 public class CustomerDAO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    int customer_id; // SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
 
-    private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("hibernate");
+    @Column(name="store_id")
+    int storeID; // TINYINT UNSIGNED NOT NULL,
 
-    public List<Customer> readTable(){
-        EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
-        EntityTransaction transaction = null;
-        List<Customer> list = null;
-        try {
-            transaction = entityManager.getTransaction();
-            transaction.begin();
+    @Column(name="first_name")
+    String firstName; // VARCHAR(45) NOT NULL,
 
-            Query query = entityManager.createQuery("SELECT c FROM Customer c LEFT JOIN FETCH c.storeId", Customer.class);
-            list = query.getResultList();
-            query = entityManager.createQuery("SELECT c FROM Customer c LEFT JOIN FETCH c.addressId", Customer.class);
-            list = query.getResultList();
+    @Column(name="last_name")
+    String lastName; // VARCHAR(45) NOT NULL,
 
-            transaction.commit();
+    @Column(name="email")
+    String email; // VARCHAR(50) DEFAULT NULL,
 
-        }catch (Exception e){
-            if(transaction != null){
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        }finally {
-            entityManager.close();
-        }
-        return list;
+    @Column(name="address_id")
+    int addressID; // SMALLINT UNSIGNED NOT NULL,
+
+    @Column(name="active")
+    boolean active; // BOOLEAN NOT NULL DEFAULT TRUE,
+
+    @Column(name="create_date")
+    Date createDate; // DATETIME NOT NULL,
+
+    @Column(name="last_update")
+    Date last_update; // TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    public int getCustomer_id() {
+        return customer_id;
     }
 
+    public void setCustomer_id(int customer_id) {
+        this.customer_id = customer_id;
+    }
 
+    public int getStoreID() {
+        return storeID;
+    }
+
+    public void setStoreID(int storeID) {
+        this.storeID = storeID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAddressID() {
+        return addressID;
+    }
+
+    public void setAddressID(int addressID) {
+        this.addressID = addressID;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getLast_update() {
+        return last_update;
+    }
+
+    public void setLast_update(Date last_update) {
+        this.last_update = last_update;
+    }
 }
-
